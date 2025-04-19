496d2f104fc2d66a45a77b75b1cc7f56b016488c
MKirDev <mkir2001@mail.ru>
2025-04-19 23:47:57 +0700

    Moving the application theme folder to the core:ui module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ android {
 
 dependencies {
 
+    implementation(project(":core:ui"))
+
     // core
     implementation(libs.core.ktx)
 
--- core/ui/src/main/java/com/mkirdev/unsplash/core/ui/theme/Theme.kt
+++ core/ui/src/main/java/com/mkirdev/unsplash/core/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.mkirdev.unsplash.ui.theme
+package com.mkirdev.unsplash.core.ui.theme
 
 import android.app.Activity
 import android.os.Build
