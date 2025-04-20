a10f83b39be860be2cba437c08222402fe557819
MKirDev <mkir2001@mail.ru>
2025-04-20 23:12:12 +0700

    Updated Type.kt, added logo and new fonts

--- core/ui/src/main/java/com/mkirdev/unsplash/core/ui/theme/Type.kt
+++ core/ui/src/main/java/com/mkirdev/unsplash/core/ui/theme/Type.kt
@@ -1,34 +1,132 @@
 package com.mkirdev.unsplash.core.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = robotoFamily,
+        fontWeight = FontWeight.Black,
+        fontSize = 34.sp,
+        lineHeight = 43.82.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = robotoFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 27.sp,
+        lineHeight = 34.80.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = robotoFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 18.04.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = robotoFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        lineHeight = 25.sp,
+        letterSpacing = 0.32.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = robotoFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 18.04.sp,
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = robotoFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 13.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = robotoFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 15.sp,
+        lineHeight = 19.34.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = robotoFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 14.sp,
+        lineHeight = 18.04.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = robotoFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 13.sp,
+        letterSpacing = 0.26.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = robotoFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 11.sp,
+        lineHeight = 14.179.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = robotoFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 13.sp,
+        letterSpacing = 0.26.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = robotoFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 8.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = robotoFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 6.sp
     )
-    */
 )
+
+val Typography.displayLargeBold: TextStyle
+@Composable
+get() {
+    return TextStyle(
+        fontFamily = robotoFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 34.sp,
+        lineHeight = 25.sp,
+        letterSpacing = 0.68.sp
+    )
+}
+
+val Typography.headlineMediumBold: TextStyle
+@Composable
+get() {
+    return TextStyle(
+        fontFamily = robotoFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 18.04.sp,
+    )
+}
+
+val Typography.bodyLargeMedium: TextStyle
+@Composable
+get() {
+    return TextStyle(
+        fontFamily = robotoFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 18.04.sp,
+    )
+}
+
+val Typography.bodySmallWithoutLineHeight: TextStyle
+@Composable
+get() {
+    return TextStyle(
+        fontFamily = robotoFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 11.sp
+    )
+}
+
