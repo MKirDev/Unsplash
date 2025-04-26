1bc999bf379d094c63280c3e32f03675a650edfb
MKirDev <mkir2001@mail.ru>
2025-04-26 19:02:37 +0700

    Updated preview AuthScreen.kt, removed unnecessary imports in AuthScreen.kt and AuthViewModel.kt

--- features/auth_impl/src/main/java/com/mkirdev/unsplash/auth/impl/AuthScreen.kt
+++ features/auth_impl/src/main/java/com/mkirdev/unsplash/auth/impl/AuthScreen.kt
@@ -3,38 +3,28 @@ package com.mkirdev.unsplash.auth.impl
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.mkirdev.unsplash.core.ui.R
+import com.mkirdev.unsplash.core.ui.theme.UnsplashTheme
 import com.mkirdev.unsplash.core.ui.theme.bodyLargeMedium
 import com.mkirdev.unsplash.core.ui.theme.image_size_340
-import com.mkirdev.unsplash.core.ui.theme.item_height_378
 import com.mkirdev.unsplash.core.ui.theme.item_height_54
-import com.mkirdev.unsplash.core.ui.theme.padding_100
-import com.mkirdev.unsplash.core.ui.theme.padding_200
 import com.mkirdev.unsplash.core.ui.theme.padding_30
-import com.mkirdev.unsplash.core.ui.theme.padding_80
 import com.mkirdev.unsplash.core.ui.widgets.ClosableErrorField
 import com.mkirdev.unsplash.core.ui.widgets.StandardButton
 import com.mkirdev.unsplash.core.ui.widgets.StaticEmptyField
@@ -68,7 +58,12 @@ fun AuthScreen(
         StandardButton(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = padding_30, start = padding_30, end = padding_30, bottom = padding_30),
+                .padding(
+                    top = padding_30,
+                    start = padding_30,
+                    end = padding_30,
+                    bottom = padding_30
+                ),
             text = stringResource(id = R.string.auth),
             textStyle = MaterialTheme.typography.labelLarge,
             colors = ButtonDefaults.buttonColors(
@@ -113,9 +108,11 @@ object AuthScreenTags {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun AuthScreenPreview() {
-    AuthScreen(
-        uiState = AuthContract.State.Success,
-        onNotificationClick = {},
-        onAuthClick = {}
-    )
+    UnsplashTheme(dynamicColor = false) {
+        AuthScreen(
+            uiState = AuthContract.State.Success,
+            onNotificationClick = {},
+            onAuthClick = {}
+        )
+    }
 }
