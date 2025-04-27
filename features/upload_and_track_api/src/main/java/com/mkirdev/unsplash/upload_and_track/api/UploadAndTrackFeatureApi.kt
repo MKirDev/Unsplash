f9b70e7d0c6c25f5391327fc1edfe8242396b1c9
MKirDev <mkir2001@mail.ru>
2025-04-27 20:31:48 +0700

    Updated build.gradle.kts (:features:upload_and_track_api) and created UploadAndTrackFeatureApi.kt

---
+++ features/upload_and_track_api/src/main/java/com/mkirdev/unsplash/upload_and_track/api/UploadAndTrackFeatureApi.kt
@@ -0,0 +1,8 @@
+package com.mkirdev.unsplash.upload_and_track.api
+
+import androidx.compose.runtime.Composable
+
+interface UploadAndTrackFeatureApi {
+    @Composable
+    fun UploadAndTrackScreen(text: String)
+}
