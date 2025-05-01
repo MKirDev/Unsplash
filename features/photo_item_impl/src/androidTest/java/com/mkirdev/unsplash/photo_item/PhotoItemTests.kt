5c6622443f1947c6f1fa3f077077723d3d7d39d4
MKirDev <mkir2001@mail.ru>
2025-05-01 17:46:34 +0700

    Updated PhotoItemTests.kt and PreviewData.kt

--- features/photo_item_impl/src/androidTest/java/com/mkirdev/unsplash/photo_item/PhotoItemTests.kt
+++ features/photo_item_impl/src/androidTest/java/com/mkirdev/unsplash/photo_item/PhotoItemTests.kt
@@ -16,7 +16,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class PhotoItemTests {
+internal class PhotoItemTests {
 
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
--- features/photo_item_impl/src/main/java/com/mkirdev/unsplash/photo_item/preview/PreviewData.kt
+++ features/photo_item_impl/src/main/java/com/mkirdev/unsplash/photo_item/preview/PreviewData.kt
@@ -3,7 +3,7 @@ package com.mkirdev.unsplash.photo_item.preview
 import com.mkirdev.unsplash.photo_item.api.models.PhotoItemModel
 import com.mkirdev.unsplash.photo_item.api.models.UserModel
 
-internal fun createPreviewData() = PhotoItemModel(
+fun createPreviewData() = PhotoItemModel(
     id = "IOsig125yhdf",
     imageUrl = "https://images.unsplash.com/photo-1738807992185-76ab3a0573c4?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     aspectRatioImage = 2.5f,
