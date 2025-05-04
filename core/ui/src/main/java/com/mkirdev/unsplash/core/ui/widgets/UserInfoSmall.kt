801a88e1d9813abd719b443895e0b3e3c5dfd042
MKirDev <mkir2001@mail.ru>
2025-05-05 00:11:27 +0700

    Removed unnecessary import in UserInfoSmall.kt

--- core/ui/src/main/java/com/mkirdev/unsplash/core/ui/widgets/UserInfoSmall.kt
+++ core/ui/src/main/java/com/mkirdev/unsplash/core/ui/widgets/UserInfoSmall.kt
@@ -6,7 +6,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import com.mkirdev.unsplash.core.ui.R
 import com.mkirdev.unsplash.core.ui.theme.UnsplashTheme
