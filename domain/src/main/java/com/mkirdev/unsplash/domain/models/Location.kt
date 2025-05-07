635c8067299a9e5f70f469481ba57acd4721e3d6
MKirDev <mkir2001@mail.ru>
2025-05-07 23:28:39 +0700

    Updated models and created Coordinates.kt in domain module

--- domain/src/main/java/com/mkirdev/unsplash/domain/models/Location.kt
+++ domain/src/main/java/com/mkirdev/unsplash/domain/models/Location.kt
@@ -3,6 +3,5 @@ package com.mkirdev.unsplash.domain.models
 data class Location(
     val name: String,
     val city: String,
-    val latitude: Double,
-    val longitude: Double,
+    val coordinates: Coordinates
 )
