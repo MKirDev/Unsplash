ff2dea22b101685c1d484366f144f22558f63860
MKirDev <mkir2001@mail.ru>
2025-05-09 20:52:39 +0700

    Updated DetailsFeatureApiImpl.kt in details_impl module

--- features/details_impl/src/main/java/com/mkirdev/unsplash/details/api/DetailsFeatureApiImpl.kt
+++ features/details_impl/src/main/java/com/mkirdev/unsplash/details/api/DetailsFeatureApiImpl.kt
@@ -1,14 +1,64 @@
 package com.mkirdev.unsplash.details.api
 
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.mkirdev.unsplash.core.contract.viewmodel.applyEffect
+import com.mkirdev.unsplash.core.navigation.ProjectNavDestination
+import com.mkirdev.unsplash.details.impl.PhotoDetailsContract
+import com.mkirdev.unsplash.details.impl.PhotoDetailsScreen
+import com.mkirdev.unsplash.details.impl.PhotoDetailsViewModel
 
 class DetailsFeatureApiImpl : DetailsFeatureApi {
     override fun NavHostController.navigateToDetails(photoId: String) {
-        TODO("Not yet implemented")
+        navigate("${DetailsDestination.route}/${photoId}")
     }
 
     override fun NavGraphBuilder.details(onNavigateUp: () -> Unit, onNavigateBack: () -> Unit) {
-        TODO("Not yet implemented")
+        composable(
+            route = DetailsDestination.routeWithArgument,
+            arguments = DetailsDestination.arguments
+        ) {
+            val viewModel: PhotoDetailsViewModel = viewModel()
+            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+            viewModel.applyEffect(function = { effect ->
+                when (effect) {
+                    is PhotoDetailsContract.Effect.Location -> {
+
+                    }
+                    is PhotoDetailsContract.Effect.Share -> {
+
+                    }
+                    PhotoDetailsContract.Effect.PhotoFeed -> { onNavigateUp() }
+                    PhotoDetailsContract.Effect.BackPressed -> { onNavigateBack() }
+                    null -> Unit
+                }
+            })
+
+            PhotoDetailsScreen(
+                uiState = uiState,
+                onShareClick = { viewModel.handleEvent(PhotoDetailsContract.Event.ShareEvent(it)) },
+                onLikeClick = { viewModel.handleEvent(PhotoDetailsContract.Event.PhotoLikeEvent(it)) },
+                onRemoveLikeClick = { viewModel.handleEvent(PhotoDetailsContract.Event.PhotoRemoveLikeEvent(it)) },
+                onLocationClick = { viewModel.handleEvent(PhotoDetailsContract.Event.LocationEvent(it)) },
+                onDownloadClick = { viewModel.handleEvent(PhotoDetailsContract.Event.DownloadEvent(it)) },
+                onCloseFieldClick = { viewModel.handleEvent(PhotoDetailsContract.Event.FieldCloseEvent) },
+                onNavigateUp = { onNavigateUp() },
+                onNavigateBack = { onNavigateBack() }
+                )
+        }
     }
+}
+
+object DetailsDestination : ProjectNavDestination {
+    private const val argumentName = "photoId"
+    override val route: String = "details"
+    val routeWithArgument = "${route}/{${argumentName}}"
+    val arguments = listOf(navArgument(argumentName) { type = NavType.StringType })
 }
