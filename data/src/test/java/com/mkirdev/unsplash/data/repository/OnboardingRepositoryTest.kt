050f4da9c74c79d35d67392e988cfdf5e4914b88
MKirDev <mkir2001@mail.ru>
2025-05-20 00:05:19 +0700

    OnboardingRepository testing in the data module

---
+++ data/src/test/java/com/mkirdev/unsplash/data/repository/OnboardingRepositoryTest.kt
@@ -0,0 +1,90 @@
+package com.mkirdev.unsplash.data.repository
+
+import androidx.datastore.core.CorruptionException
+import com.mkirdev.unsplash.data.exceptions.OnboardingException
+import com.mkirdev.unsplash.data.storages.datastore.OnboardingStorage
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert.assertThrows
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class OnboardingRepositoryTest {
+
+    private lateinit var storage: OnboardingStorage
+    private val dispatcher = UnconfinedTestDispatcher()
+
+    @Before
+    fun setup() {
+       storage = mockk(relaxed = true)
+    }
+
+    @After
+    fun teardown() {
+        clearAllMocks()
+    }
+
+    @Test
+    fun addFlagFromStorageIsRequestedOnlyOnce() = runTest(dispatcher) {
+        val repository = OnboardingRepositoryImpl(
+            onboardingStorage = storage,
+            dispatcher = dispatcher,
+        )
+
+        val expected = true
+
+        repository.saveFlag(isOnboardingEnded = true)
+
+        coVerify(exactly = 1) { storage.addFlag(expected) }
+    }
+
+    @Test
+    fun getFlagFromStorageIsRequestedOnlyOnce() = runTest(dispatcher) {
+        val repository = OnboardingRepositoryImpl(
+            onboardingStorage = storage,
+            dispatcher = dispatcher,
+        )
+
+        repository.getFlag()
+
+        coVerify(exactly = 1) { storage.getFlag() }
+    }
+
+    @Test
+    fun clearFromStorageIsRequestedOnlyOnce() = runTest(dispatcher) {
+        val repository = OnboardingRepositoryImpl(
+            onboardingStorage = storage,
+            dispatcher = dispatcher,
+        )
+
+        repository.clear()
+
+        coVerify(exactly = 1) { storage.clear() }
+    }
+
+    @Test
+    fun getFlagWithException() = runTest(dispatcher) {
+        val repository = OnboardingRepositoryImpl(
+            onboardingStorage = storage,
+            dispatcher = dispatcher,
+        )
+
+        coEvery { storage.getFlag() } throws CorruptionException("Datastore exception")
+
+        assertThrows(
+            "",
+            OnboardingException.GetFlagException::class.java
+        ) {
+            runTest(dispatcher) { repository.getFlag() }
+        }
+    }
+
+
+}
