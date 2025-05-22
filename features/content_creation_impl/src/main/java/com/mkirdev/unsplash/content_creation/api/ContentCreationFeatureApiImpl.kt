ea71d6350b9d9bb6267f058246613fad17b5467c
MKirDev <mkir2001@mail.ru>
2025-05-22 23:33:54 +0700

    Updated content_creation_api and content_creation_impl modules

--- features/content_creation_impl/src/main/java/com/mkirdev/unsplash/content_creation/api/ContentCreationFeatureApiImpl.kt
+++ features/content_creation_impl/src/main/java/com/mkirdev/unsplash/content_creation/api/ContentCreationFeatureApiImpl.kt
@@ -1,10 +1,11 @@
 package com.mkirdev.unsplash.content_creation.api
 
 import androidx.compose.runtime.Composable
+import com.mkirdev.unsplash.content_creation.impl.ContentCreationScreen
 import javax.inject.Inject
 
 class ContentCreationFeatureApiImpl @Inject constructor() : ContentCreationFeatureApi {
     @Composable
-    override fun ContentCreationScreen(text: String): Unit =
+    override fun ContentCreationFeature(text: String): Unit =
         ContentCreationScreen(text = text)
 }
