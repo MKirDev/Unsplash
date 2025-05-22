163c89f6000fce48131909839c174e212268cce2
MKirDev <mkir2001@mail.ru>
2025-05-22 23:35:53 +0700

    Updated upload_and_track_api and upload_and_track_impl modules

--- features/upload_and_track_impl/src/main/java/com/mkirdev/unsplash/upload_and_track/api/UploadAndTrackFeatureApiImpl.kt
+++ features/upload_and_track_impl/src/main/java/com/mkirdev/unsplash/upload_and_track/api/UploadAndTrackFeatureApiImpl.kt
@@ -1,10 +1,11 @@
 package com.mkirdev.unsplash.upload_and_track.api
 
 import androidx.compose.runtime.Composable
+import com.mkirdev.unsplash.upload_and_track.impl.UploadAndTrackScreen
 import javax.inject.Inject
 
 class UploadAndTrackFeatureApiImpl @Inject constructor() : UploadAndTrackFeatureApi {
     @Composable
-    override fun UploadAndTrackScreen(text: String): Unit =
+    override fun UploadAndTrackFeature(text: String): Unit =
         UploadAndTrackScreen(text = text)
 }
