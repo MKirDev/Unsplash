50e7654daa4db8215f1605a9d3aeb37ea918e021
MKirDev <mkir2001@mail.ru>
2025-05-22 23:35:09 +0700

    Updated social_collections_api and social_collections_impl modules

--- features/social_collections_impl/src/main/java/com/mkirdev/unsplash/social_collections/api/SocialCollectionsFeatureApiImpl.kt
+++ features/social_collections_impl/src/main/java/com/mkirdev/unsplash/social_collections/api/SocialCollectionsFeatureApiImpl.kt
@@ -1,9 +1,10 @@
 package com.mkirdev.unsplash.social_collections.api
 
 import androidx.compose.runtime.Composable
+import com.mkirdev.unsplash.social_collections.impl.SocialCollectionsScreen
 import javax.inject.Inject
 
 class SocialCollectionsFeatureApiImpl @Inject constructor() : SocialCollectionsFeatureApi {
     @Composable
-    override fun SocialCollectionsScreen(text: String): Unit = SocialCollectionsScreen(text = text)
+    override fun SocialCollectionsFeature(text: String): Unit = SocialCollectionsScreen(text = text)
 }
