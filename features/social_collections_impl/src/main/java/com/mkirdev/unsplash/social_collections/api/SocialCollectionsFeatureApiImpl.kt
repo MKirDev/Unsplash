fff85f961037429ed90e1961a389d06cdadbb232
MKirDev <mkir2001@mail.ru>
2025-05-22 17:25:16 +0700

    Updated implementation of features for onboarding

--- features/social_collections_impl/src/main/java/com/mkirdev/unsplash/social_collections/api/SocialCollectionsFeatureApiImpl.kt
+++ features/social_collections_impl/src/main/java/com/mkirdev/unsplash/social_collections/api/SocialCollectionsFeatureApiImpl.kt
@@ -1,8 +1,9 @@
 package com.mkirdev.unsplash.social_collections.api
 
 import androidx.compose.runtime.Composable
+import javax.inject.Inject
 
-class SocialCollectionsFeatureApiImpl : SocialCollectionsFeatureApi {
+class SocialCollectionsFeatureApiImpl @Inject constructor() : SocialCollectionsFeatureApi {
     @Composable
     override fun SocialCollectionsScreen(text: String): Unit = SocialCollectionsScreen(text = text)
 }
