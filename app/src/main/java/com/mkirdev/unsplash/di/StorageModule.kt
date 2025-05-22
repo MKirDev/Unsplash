c293aa680abf17447b3c9ae312e2da8b34b45e70
MKirDev <mkir2001@mail.ru>
2025-05-22 17:26:17 +0700

    Updated DI

--- app/src/main/java/com/mkirdev/unsplash/di/StorageModule.kt
+++ app/src/main/java/com/mkirdev/unsplash/di/StorageModule.kt
@@ -2,16 +2,29 @@ package com.mkirdev.unsplash.di
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import com.mkirdev.unsplash.app.DataStoreManager
 import com.mkirdev.unsplash.data.storages.datastore.OnboardingStorage
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 import javax.inject.Singleton
+
+private const val ONBOARDING_STORAGE = "onboardingStorage"
 
 @Module
 class StorageModule {
+
+    @Provides
+    @Named(ONBOARDING_STORAGE)
+    fun provideOnboardingDataStore(dataStoreManager: DataStoreManager): DataStore<Preferences> {
+        return dataStoreManager.onboardingDataStore
+    }
+
     @Singleton
     @Provides
-    fun provideOnboardingStorage(dataStore: DataStore<Preferences>): OnboardingStorage {
+    fun provideOnboardingStorage(
+        @Named(ONBOARDING_STORAGE) dataStore: DataStore<Preferences>
+    ): OnboardingStorage {
         return OnboardingStorage(dataStore = dataStore)
     }
 }
