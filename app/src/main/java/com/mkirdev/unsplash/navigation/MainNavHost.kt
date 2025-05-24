a8f518c13a1812fe67daadadd2930ae7796b4f56
MKirDev <mkir2001@mail.ru>
2025-05-24 13:37:07 +0700

    Updated MainActivity with AndroidManifest.xml (:app), created MainNavHost.kt and MainViewModel.kt

---
+++ app/src/main/java/com/mkirdev/unsplash/navigation/MainNavHost.kt
@@ -0,0 +1,36 @@
+package com.mkirdev.unsplash.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.mkirdev.unsplash.di.DaggerProvider
+import com.mkirdev.unsplash.onboarding.api.OnboardingDestination
+
+@Composable
+fun MainNavHost(
+    navController: NavHostController = rememberNavController(),
+    viewModel: MainViewModel = viewModel()
+) {
+
+    val onboardingFeatureApi by remember {
+        mutableStateOf(DaggerProvider.appComponent.onboardingFeatureApi)
+    }
+
+
+    NavHost(
+        navController = navController,
+        startDestination = OnboardingDestination.route
+    ) {
+        with(onboardingFeatureApi) {
+            onboarding(
+                onNavigateToAuth = {
+                }
+            )
+        }
+    }
+}
