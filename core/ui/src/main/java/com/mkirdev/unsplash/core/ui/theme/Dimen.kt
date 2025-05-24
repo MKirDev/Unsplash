2074c9f70c6a5c8643c366adfe0f706e65b24dd1
MKirDev <mkir2001@mail.ru>
2025-05-24 13:35:41 +0700

    Updated core:ui resources

--- core/ui/src/main/java/com/mkirdev/unsplash/core/ui/theme/Dimen.kt
+++ core/ui/src/main/java/com/mkirdev/unsplash/core/ui/theme/Dimen.kt
@@ -16,9 +16,13 @@ val padding_28 = 28.dp
 val padding_30 = 30.dp
 val padding_48 = 48.dp
 val padding_60 = 60.dp
+val padding_70 = 70.dp
 val padding_80 = 100.dp
 val padding_100 = 100.dp
 val padding_150 = 150.dp
+val padding_190 = 190.dp
+val padding_250 = 250.dp
+val padding_320 = 320.dp
 
 val space_1 = 1.dp
 val space_4 = 4.dp
@@ -30,6 +34,7 @@ val space_24 = 24.dp
 val space_30 = 30.dp
 val space_40 = 40.dp
 val space_60 = 60.dp
+val space_100 = 100.dp
 
 val item_width_12 = 12.dp
 val item_width_20 = 20.dp
@@ -40,8 +45,10 @@ val item_width_158 = 158.dp
 val item_width_360 = 360.dp
 
 val item_height_14 = 14.dp
+val item_height_44 = 44.dp
 val item_height_54 = 54.dp
 val item_height_138 = 138.dp
+val item_height_238 = 238.dp
 val item_height_378 = 378.dp
 
 val item_size_18 = 18.dp
@@ -59,3 +66,5 @@ val rounded_corner_0 = 0.dp
 val rounded_corner_100 = 100.dp
 
 val border_1 = 1.2.dp
+
+val elevation_1 = 1.dp
