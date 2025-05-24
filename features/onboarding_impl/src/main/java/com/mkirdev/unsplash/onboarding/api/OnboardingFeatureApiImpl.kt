6dbee3b201bfb488e7a97f3c1c7d751227d18af5
MKirDev <mkir2001@mail.ru>
2025-05-25 01:13:41 +0700

    Created api, di, impl, widgets of the onboarding_impl module

---
+++ features/onboarding_impl/src/main/java/com/mkirdev/unsplash/onboarding/api/OnboardingFeatureApiImpl.kt
@@ -0,0 +1,75 @@
+package com.mkirdev.unsplash.onboarding.api
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.mkirdev.unsplash.core.contract.viewmodel.applyEffect
+import com.mkirdev.unsplash.onboarding.di.DaggerOnboardingComponent
+import com.mkirdev.unsplash.onboarding.di.OnboardingDependenciesProvider
+import com.mkirdev.unsplash.onboarding.impl.OnboardingContract
+import com.mkirdev.unsplash.onboarding.impl.OnboardingScreen
+import com.mkirdev.unsplash.onboarding.impl.OnboardingViewModel
+import com.mkirdev.unsplash.onboarding.impl.OnboardingViewModelFactory
+import com.mkirdev.unsplash.onboarding.navigation.OnboardingDestination
+import javax.inject.Inject
+
+class OnboardingFeatureApiImpl @Inject constructor(): OnboardingFeatureApi {
+    override fun NavHostController.navigateToOnboarding() {
+        navigate(route = OnboardingDestination.route)
+    }
+
+    override fun NavGraphBuilder.onboarding(onNavigateToAuth: () -> Unit) {
+        composable(route = OnboardingDestination.route) {
+
+            val onboardingComponent by remember {
+                mutableStateOf(
+                    DaggerOnboardingComponent.builder()
+                        .addDependencies(OnboardingDependenciesProvider.dependencies)
+                        .build()
+                )
+            }
+
+            val viewModel: OnboardingViewModel = viewModel(
+                factory = OnboardingViewModelFactory(
+                    saveOnboardingFlagUseCase = onboardingComponent.saveOnboardingFlagUseCase,
+                    getOnboardingFlagUseCase = onboardingComponent.getOnboardingFlagUseCase
+                )
+            )
+
+            val contentCreationFeatureApi by remember {
+                mutableStateOf(onboardingComponent.contentCreationFeatureApi)
+            }
+
+            val socialCollectionsFeatureApi by remember {
+                mutableStateOf(onboardingComponent.socialCollectionsFeatureApi)
+            }
+
+            val uploadAndTrackFeatureApi by remember {
+                mutableStateOf(onboardingComponent.uploadAndTrackFeatureApi)
+            }
+
+            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+            viewModel.applyEffect(function = { effect ->
+                when (effect) {
+                    OnboardingContract.Effect.Auth -> onNavigateToAuth()
+                    null -> Unit
+                }
+            })
+
+            OnboardingScreen(
+                uiState = uiState,
+                contentCreationFeatureApi = contentCreationFeatureApi,
+                socialCollectionsFeatureApi = socialCollectionsFeatureApi,
+                uploadAndTrackFeatureApi = uploadAndTrackFeatureApi,
+                onAuthClick = { viewModel.handleEvent(OnboardingContract.Event.AuthOpenedEvent) },
+                onCloseFieldClick = { viewModel.handleEvent(OnboardingContract.Event.FieldClosedEvent) }
+            )
+        }
+    }
+}
---
+++ features/onboarding_impl/src/main/java/com/mkirdev/unsplash/onboarding/impl/OnboardingViewModel.kt
@@ -0,0 +1,102 @@
+package com.mkirdev.unsplash.onboarding.impl
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.mkirdev.unsplash.domain.usecases.onboarding.GetOnboardingFlagUseCase
+import com.mkirdev.unsplash.domain.usecases.onboarding.SaveOnboardingFlagUseCase
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class OnboardingViewModel(
+    private val saveOnboardingFlagUseCase: SaveOnboardingFlagUseCase,
+    private val getOnboardingFlagUseCase: GetOnboardingFlagUseCase
+) : ViewModel(), OnboardingContract {
+
+    private val _uiState = MutableStateFlow<OnboardingContract.State>(OnboardingContract.State.Idle)
+    override val uiState: StateFlow<OnboardingContract.State> = _uiState.asStateFlow()
+
+    private val _effect = MutableStateFlow<OnboardingContract.Effect?>(null)
+    override val effect: StateFlow<OnboardingContract.Effect?> = _effect.asStateFlow()
+
+    init {
+        _uiState.update {
+            OnboardingContract.State.Onboarding(
+                pages = persistentListOf(
+                    OnboardingPage.First, OnboardingPage.Second, OnboardingPage.Third
+                ),
+                isError = false
+            )
+        }
+    }
+
+    override fun handleEvent(event: OnboardingContract.Event) {
+        when (event) {
+            OnboardingContract.Event.AuthOpenedEvent -> onAuth()
+            OnboardingContract.Event.FieldClosedEvent -> onCloseFieldClick()
+        }
+    }
+
+    override fun resetEffect() {
+        _effect.update { null }
+    }
+
+    private fun onAuth() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                saveOnboardingFlagUseCase.execute(true)
+                if ((_uiState.value as OnboardingContract.State.Onboarding).isError) {
+                    _uiState.update {
+                        (it as OnboardingContract.State.Onboarding).copy(
+                            isError = false
+                        )
+                    }
+                }
+            } catch (t: Throwable) {
+                _uiState.update {
+                    (it as OnboardingContract.State.Onboarding).copy(
+                        isError = true
+                    )
+                }
+            } finally {
+                val isOnboardingEnded = getFlag()
+                withContext(Dispatchers.Main) {
+                    if (isOnboardingEnded) _effect.update { OnboardingContract.Effect.Auth }
+                }
+            }
+        }
+    }
+
+    private suspend fun getFlag(): Boolean {
+        return getOnboardingFlagUseCase.execute()
+    }
+
+    private fun onCloseFieldClick() {
+        _uiState.update {
+            (it as OnboardingContract.State.Onboarding).copy(
+                isError = false
+            )
+        }
+    }
+
+}
+
+class OnboardingViewModelFactory(
+    private val saveOnboardingFlagUseCase: SaveOnboardingFlagUseCase,
+    private val getOnboardingFlagUseCase: GetOnboardingFlagUseCase
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+        return OnboardingViewModel(
+            saveOnboardingFlagUseCase = saveOnboardingFlagUseCase,
+            getOnboardingFlagUseCase = getOnboardingFlagUseCase
+        ) as T
+    }
+}
