2eeb5c7372f3ad6b60a333b5efe010b0b1f2b25e
MKirDev <mkir2001@mail.ru>
2025-05-26 23:22:12 +0700

    Updated Contract.kt in the :core:contract:usecase module

--- core/contract/usecase/src/main/java/com/mkirdev/unsplash/core/contract/usecase/Contract.kt
+++ core/contract/usecase/src/main/java/com/mkirdev/unsplash/core/contract/usecase/Contract.kt
@@ -11,3 +11,11 @@ interface UseCaseWithoutParamAndResult {
 interface UseCaseWithResult<R> {
     suspend fun execute(): R
 }
+
+interface UseCaseWithParamAndResult<P, R> {
+    suspend fun execute(param: P): R
+}
+
+interface UseCaseWithParams<P1, P2> {
+    suspend fun execute(firstParam: P1, secondParam: P2)
+}
