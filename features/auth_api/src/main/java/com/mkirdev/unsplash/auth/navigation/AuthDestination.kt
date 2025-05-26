789fc5c380c1a623a79004e05ad1ba0dff25021d
MKirDev <mkir2001@mail.ru>
2025-05-26 17:58:13 +0700

    Created AuthDestination in features:auth_api module

---
+++ features/auth_api/src/main/java/com/mkirdev/unsplash/auth/navigation/AuthDestination.kt
@@ -0,0 +1,7 @@
+package com.mkirdev.unsplash.auth.navigation
+
+import com.mkirdev.unsplash.core.navigation.ProjectNavDestination
+
+object AuthDestination : ProjectNavDestination {
+    override val route: String = "start"
+}
