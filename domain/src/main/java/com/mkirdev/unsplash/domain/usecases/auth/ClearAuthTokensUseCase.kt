cb7e946554392a61500d333e0d1672ee704d62e2
MKirDev <mkir2001@mail.ru>
2025-05-27 20:10:58 +0700

    Usecases for auth have been renamed

--- domain/src/main/java/com/mkirdev/unsplash/domain/usecases/auth/ClearAuthTokensUseCase.kt
+++ domain/src/main/java/com/mkirdev/unsplash/domain/usecases/auth/ClearAuthTokensUseCase.kt
@@ -3,7 +3,7 @@ package com.mkirdev.unsplash.domain.usecases.auth
 import com.mkirdev.unsplash.core.contract.usecase.UseCaseWithoutParamAndResult
 import com.mkirdev.unsplash.domain.repository.AuthRepository
 
-class ClearAuthTokenUseCase(
+class ClearAuthTokensUseCase(
     private val repository: AuthRepository
 ) : UseCaseWithoutParamAndResult {
     override suspend fun execute() {
--- domain/src/main/java/com/mkirdev/unsplash/domain/usecases/auth/PerformTokensRequestUseCase.kt
+++ domain/src/main/java/com/mkirdev/unsplash/domain/usecases/auth/PerformTokensRequestUseCase.kt
@@ -3,11 +3,11 @@ package com.mkirdev.unsplash.domain.usecases.auth
 import com.mkirdev.unsplash.core.contract.usecase.UseCaseWithParam
 import com.mkirdev.unsplash.domain.repository.AuthRepository
 
-class PerformTokenRequestUseCase(
+class PerformTokensRequestUseCase(
     private val repository: AuthRepository
 ) : UseCaseWithParam<String> {
     override suspend fun execute(tokenRequestJson: String) {
-        repository.performTokenRequest(
+        repository.performTokensRequest(
             tokenRequestJson = tokenRequestJson
         )
     }
