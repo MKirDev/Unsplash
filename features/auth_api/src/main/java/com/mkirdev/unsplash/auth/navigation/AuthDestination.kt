319a7b250ba6ae53f03eb7efab01751c06337311
MKirDev <mkir2001@mail.ru>
2025-06-03 22:56:50 +0700

    Updated AuthDestination and AuthFeatureApi in auth_api module

--- features/auth_api/src/main/java/com/mkirdev/unsplash/auth/navigation/AuthDestination.kt
+++ features/auth_api/src/main/java/com/mkirdev/unsplash/auth/navigation/AuthDestination.kt
@@ -9,7 +9,10 @@ object AuthDestination : ProjectNavDestination {
     const val codeArg = "code"
     override val route: String = "auth?code={$codeArg}"
     val routeWithArgs = "$route/{$codeArg}"
-    val arguments = listOf(navArgument(codeArg) { type = NavType.StringType })
+    val arguments = listOf(navArgument(codeArg) {
+        type = NavType.StringType
+        nullable = true
+    })
     fun deeplink(schema: String, host: String, path: String) = listOf(navDeepLink {
         uriPattern = "$schema://$host$path?code={${codeArg}}"
     })
