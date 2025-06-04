06aac82f19871a498c3b0c6e451f6e46bfad7eb3
MKirDev <mkir2001@mail.ru>
2025-06-04 14:54:03 +0700

    Created PhotoFeedNetwork in the data module

---
+++ data/src/main/java/com/mkirdev/unsplash/data/network/photo/models/list/PhotoFeedNetwork.kt
@@ -0,0 +1,25 @@
+package com.mkirdev.unsplash.data.network.photo.models.list
+
+import com.google.gson.annotations.SerializedName
+import com.mkirdev.unsplash.data.network.photo.models.common.LinksNetwork
+import com.mkirdev.unsplash.data.network.photo.models.common.UrlNetwork
+import com.mkirdev.unsplash.data.network.photo.models.common.UserNetwork
+
+data class PhotoFeedNetwork(
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("width")
+    val width: Int,
+    @SerializedName("height")
+    val height: Int,
+    @SerializedName("urls")
+    val imageUrl: UrlNetwork,
+    @SerializedName("links")
+    val downloadLink: LinksNetwork,
+    @SerializedName("likes")
+    val likes: Int,
+    @SerializedName("liked_by_user")
+    val likedByUser: Boolean,
+    @SerializedName("user")
+    val user: UserNetwork
+)
