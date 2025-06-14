1ec0f43a659cf3cde7e35d421c93cf53d80d53b6
MKirDev <mkir2001@mail.ru>
2025-06-14 22:51:30 +0700

    Updated Navigation.kt in the core:navigation module

--- core/navigation/src/main/java/com/mkirdev/unsplash/core/navigation/Navigation.kt
+++ core/navigation/src/main/java/com/mkirdev/unsplash/core/navigation/Navigation.kt
@@ -1,5 +1,27 @@
 package com.mkirdev.unsplash.core.navigation
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+
 interface ProjectNavDestination {
     val route: String
+}
+
+interface TopLevelDestination : ProjectNavDestination {
+    @get:DrawableRes
+    val iconId: Int
+    @get:StringRes
+    val titleId: Int
+}
+
+fun NavHostController.navigateSingleDestinationTo(route: String) {
+    navigate(route) {
+        popUpTo(this@navigateSingleDestinationTo.graph.findStartDestination().id) {
+            saveState = true
+        }
+        launchSingleTop = true
+        restoreState = true
+    }
 }
