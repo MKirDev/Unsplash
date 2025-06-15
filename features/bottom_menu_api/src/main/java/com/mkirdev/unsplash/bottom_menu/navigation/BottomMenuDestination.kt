8b8671efc98d6c1cf274ad059d83d8fe86b423bf
MKirDev <mkir2001@mail.ru>
2025-06-15 22:57:14 +0700

    Created api and navigation of the bottom_menu_api module

---
+++ features/bottom_menu_api/src/main/java/com/mkirdev/unsplash/bottom_menu/navigation/BottomMenuDestination.kt
@@ -0,0 +1,8 @@
+package com.mkirdev.unsplash.bottom_menu.navigation
+
+import com.mkirdev.unsplash.core.navigation.ProjectNavDestination
+
+object BottomMenuDestination : ProjectNavDestination {
+    override val route: String = "bottom_menu"
+    val graph: String = "bottom_menu_graph"
+}
