822477afb36ae3d4b6bf3115273ecfbcd7e4c625
MKirDev <mkir2001@mail.ru>
2025-06-21 05:00:27 +0700

    Updated DetailsPreviewData.kt in the details_impl module

--- features/details_impl/src/main/java/com/mkirdev/unsplash/details/preview/DetailsPreviewData.kt
+++ features/details_impl/src/main/java/com/mkirdev/unsplash/details/preview/DetailsPreviewData.kt
@@ -19,7 +19,9 @@ fun createPhotoDetailsPreview() = DetailsModel(
 private fun createPhotoItemPreview() = PhotoItemModel(
     id = "IOsig125yhdf",
     imageUrl = "https://images.unsplash.com/photo-1738807992185-76ab3a0573c4?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    aspectRatioImage = 0.5f,
+    width = 800,
+    height = 1600,
+    aspectRatioImage = 1f,
     user = UserModel(
         name = "Spenser Sembrat",
         username = "spensersembrat",
