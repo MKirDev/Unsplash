9144419874559204023e888b34a6e5f61adc5084
MKirDev <mkir2001@mail.ru>
2025-06-26 22:33:43 +0700

    Updated preview in the details_impl module

--- features/details_impl/src/main/java/com/mkirdev/unsplash/details/preview/DetailsPreviewData.kt
+++ features/details_impl/src/main/java/com/mkirdev/unsplash/details/preview/DetailsPreviewData.kt
@@ -1,5 +1,6 @@
 package com.mkirdev.unsplash.details.preview
 
+import androidx.compose.ui.unit.dp
 import com.mkirdev.unsplash.details.models.CoordinatesModel
 import com.mkirdev.unsplash.details.models.DetailsModel
 import com.mkirdev.unsplash.details.models.ExifModel
@@ -19,8 +20,8 @@ fun createPhotoDetailsPreview() = DetailsModel(
 private fun createPhotoItemPreview() = PhotoItemModel(
     id = "IOsig125yhdf",
     imageUrl = "https://images.unsplash.com/photo-1738807992185-76ab3a0573c4?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    width = 800,
-    height = 1600,
+    width = 100.dp,
+    height = 250.dp,
     aspectRatioImage = 1f,
     user = UserModel(
         name = "Spenser Sembrat",
