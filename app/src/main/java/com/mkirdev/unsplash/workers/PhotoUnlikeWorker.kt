474f2e0928103ece3b93db83fed51a0adace1a85
MKirDev <mkir2001@mail.ru>
2025-06-30 22:41:55 +0700

    Created workers in the app module

---
+++ app/src/main/java/com/mkirdev/unsplash/workers/PhotoUnlikeWorker.kt
@@ -0,0 +1,54 @@
+package com.mkirdev.unsplash.workers
+
+import android.content.Context
+import androidx.work.Constraints
+import androidx.work.CoroutineWorker
+import androidx.work.ExistingWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import com.mkirdev.unsplash.di.DaggerProvider
+
+private const val WORKER_TAG = "PHOTO.UNLIKE.WORKER"
+private const val WORKER_NAME = "UNLIKED PHOTO"
+private const val DATA = "DATA"
+private const val EMPTY_ID = ""
+
+class PhotoUnlikeWorker(
+    private val context: Context,
+    workerParams: WorkerParameters
+) : CoroutineWorker(context, workerParams) {
+
+    private val photosApi = DaggerProvider.appComponent.photosApi
+    override suspend fun doWork(): Result {
+        return try {
+            val photoId = inputData.getString(DATA) ?: EMPTY_ID
+            photosApi.unLikePhoto(photoId)
+            Result.success()
+        } catch (e: Exception) {
+            Result.retry()
+        }
+    }
+
+    companion object {
+        private val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        private fun createWorkRequest(photoId: String): OneTimeWorkRequest {
+            return OneTimeWorkRequestBuilder<PhotoLikeWorker>()
+                .setInputData(workDataOf(DATA to photoId))
+                .addTag(WORKER_TAG)
+                .setConstraints(constraints)
+                .build()
+        }
+
+        internal fun enqueueUniqueWork(workManager: WorkManager, photoId: String?) {
+            val workRequest = createWorkRequest(photoId ?: EMPTY_ID)
+            workManager.enqueueUniqueWork(WORKER_NAME, ExistingWorkPolicy.REPLACE, workRequest)
+        }
+    }
+}
