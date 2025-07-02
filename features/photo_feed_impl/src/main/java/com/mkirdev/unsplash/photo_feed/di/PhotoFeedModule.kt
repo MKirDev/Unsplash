83e532af2912175fe9ad6c17499b5a53be9620c8
MKirDev <mkir2001@mail.ru>
2025-07-02 15:46:22 +0700

    Updated api, di and impl in the photo_feed_impl module

--- features/photo_feed_impl/src/main/java/com/mkirdev/unsplash/photo_feed/di/PhotoFeedModule.kt
+++ features/photo_feed_impl/src/main/java/com/mkirdev/unsplash/photo_feed/di/PhotoFeedModule.kt
@@ -1,9 +1,9 @@
 package com.mkirdev.unsplash.photo_feed.di
 
 import com.mkirdev.unsplash.domain.usecases.photos.GetPhotosUseCase
-import com.mkirdev.unsplash.domain.usecases.photos.LikePhotoUseCase
+import com.mkirdev.unsplash.domain.usecases.photos.LikePhotoLocalUseCase
 import com.mkirdev.unsplash.domain.usecases.photos.SearchPhotosUseCase
-import com.mkirdev.unsplash.domain.usecases.photos.UnlikePhotoUseCase
+import com.mkirdev.unsplash.domain.usecases.photos.UnlikePhotoLocalUseCase
 import com.mkirdev.unsplash.photo_feed.impl.PhotoFeedViewModelFactory
 import dagger.Module
 import dagger.Provides
@@ -14,14 +14,14 @@ internal class PhotoFeedModule {
     @Provides
     fun providePhotoFeedViewModelFactory(
         getPhotosUseCase: GetPhotosUseCase,
-        likePhotoUseCase: LikePhotoUseCase,
-        unlikePhotoUseCase: UnlikePhotoUseCase,
+        likePhotoLocalUseCase: LikePhotoLocalUseCase,
+        unlikePhotoLocalUseCase: UnlikePhotoLocalUseCase,
         searchPhotosUseCase: SearchPhotosUseCase
     ): PhotoFeedViewModelFactory {
         return PhotoFeedViewModelFactory(
             getPhotosUseCase = getPhotosUseCase,
-            likePhotoUseCase = likePhotoUseCase,
-            unlikePhotoUseCase = unlikePhotoUseCase,
+            likePhotoLocalUseCase = likePhotoLocalUseCase,
+            unlikePhotoLocalUseCase = unlikePhotoLocalUseCase,
             searchPhotosUseCase = searchPhotosUseCase
         )
     }
