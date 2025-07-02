ae961609d797b92bcb3ed8aab60061eb16bf5702
MKirDev <mkir2001@mail.ru>
2025-07-02 15:39:25 +0700

    Updated PhotosStorage in the data module

--- data/src/main/java/com/mkirdev/unsplash/data/storages/datastore/photos/PhotosStorage.kt
+++ data/src/main/java/com/mkirdev/unsplash/data/storages/datastore/photos/PhotosStorage.kt
@@ -1,66 +1,45 @@
 package com.mkirdev.unsplash.data.storages.datastore.photos
 
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
-
-private const val LIKE_KEY = "STORAGE_PHOTOS_LIKE_KEY"
-private const val UNLIKE_KEY = "STORAGE_PHOTOS_UNLIKE_KEY"
-private const val DOWNLOAD_KEY = "STORAGE_PHOTOS_DOWNLOAD_KEY"
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 private const val EMPTY_STRING = ""
-class PhotosStorage(
-    private val dataStore: DataStore<Preferences>
-) {
-    private val likeKey: Preferences.Key<String> = stringPreferencesKey(LIKE_KEY)
+class PhotosStorage {
 
-    private val unlikeKey: Preferences.Key<String> = stringPreferencesKey(UNLIKE_KEY)
+    private val likedPhoto: MutableStateFlow<String> = MutableStateFlow(EMPTY_STRING)
+    private val unlikedPhoto: MutableStateFlow<String> = MutableStateFlow(EMPTY_STRING)
+    private val downloadLink: MutableStateFlow<String> = MutableStateFlow(EMPTY_STRING)
 
-    private val downloadKey: Preferences.Key<String> = stringPreferencesKey(DOWNLOAD_KEY)
-
-    suspend fun addLikedPhoto(id: String) {
-        dataStore.edit {
-            it[likeKey] = id
-        }
+    fun addLikedPhoto(id: String) {
+        likedPhoto.update { id }
     }
 
     fun getLikedPhoto(): Flow<String> {
-        return dataStore.data.map {
-            it[likeKey] ?: EMPTY_STRING
-        }
+        return likedPhoto.asStateFlow()
     }
 
-    suspend fun addUnlikedPhoto(id: String) {
-        dataStore.edit {
-            it[unlikeKey] = id
-        }
+    fun addUnlikedPhoto(id: String) {
+        unlikedPhoto.update { id }
     }
 
     fun getUnlikedPhoto(): Flow<String> {
-        return dataStore.data.map {
-            it[unlikeKey] ?: EMPTY_STRING
-        }
+        return unlikedPhoto.asStateFlow()
     }
 
-    suspend fun addDownloadLink(link: String) {
-        dataStore.edit {
-            it[downloadKey] = link
-        }
+    fun addDownloadLink(link: String) {
+        downloadLink.update { link }
     }
 
     fun getDownloadLink(): Flow<String> {
-        return dataStore.data.map {
-            it[downloadKey] ?: EMPTY_STRING
-        }
+        return downloadLink.asStateFlow()
     }
 
     suspend fun clear() {
-        dataStore.edit {
-            it.clear()
-        }
+        likedPhoto.update { EMPTY_STRING }
+        unlikedPhoto.update { EMPTY_STRING }
+        downloadLink.update { EMPTY_STRING }
     }
 
 }
