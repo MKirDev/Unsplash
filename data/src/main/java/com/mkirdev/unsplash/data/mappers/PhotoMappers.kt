37ec98d53d306399076e494ac24fbcc4540e336a
MKirDev <mkir2001@mail.ru>
2025-07-15 12:58:07 +0700

    Updated mappers in the data module

--- data/src/main/java/com/mkirdev/unsplash/data/mappers/PhotoMappers.kt
+++ data/src/main/java/com/mkirdev/unsplash/data/mappers/PhotoMappers.kt
@@ -1,38 +1,7 @@
 package com.mkirdev.unsplash.data.mappers
 
 import com.mkirdev.unsplash.data.network.photos.models.details.PhotoNetwork
-import com.mkirdev.unsplash.data.storages.database.entities.PhotoEntity
-import com.mkirdev.unsplash.data.storages.database.entities.PhotoReactionsEntity
-import com.mkirdev.unsplash.data.storages.database.entities.ReactionsTypeEntity
 import com.mkirdev.unsplash.domain.models.Photo
-
-
-//internal fun PhotoNetwork.toPhotoEntity(): PhotoEntity {
-//    return PhotoEntity(
-//        id = id,
-//        width = width,
-//        height = height,
-//        imageUrl = imageUrl.toRegular(),
-//        htmlLink = links.toHtml(),
-//        downloadLink = links.toDownload(),
-//        likes = likes,
-//        userId = user.id
-//    )
-//}
-
-internal fun PhotoNetwork.toReactionTypeEntity(): ReactionsTypeEntity {
-    return ReactionsTypeEntity(
-        id = id,
-        liked = if (likedByUser) 1 else 0
-    )
-}
-
-internal fun PhotoNetwork.toPhotoReactionsEntity(): PhotoReactionsEntity {
-    return PhotoReactionsEntity(
-        photoId = id,
-        reactionsId = id
-    )
-}
 
 internal fun PhotoNetwork.toDomain(): Photo {
     return Photo(
@@ -47,6 +16,7 @@ internal fun PhotoNetwork.toDomain(): Photo {
         location = location?.toDomain(),
         exif = exif?.toDomain(),
         tags = if (tags.isNullOrEmpty()) null else tags.map { it.toTitle() },
-        downloads = downloads
+        downloads = downloads,
+        position = 0
     )
 }
