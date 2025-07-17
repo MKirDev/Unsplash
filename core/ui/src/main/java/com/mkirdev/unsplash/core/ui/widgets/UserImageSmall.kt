fca7927a4ab08a43c2a7f0a649709d2bca1edcf2
MKirDev <mkir2001@mail.ru>
2025-07-17 12:57:46 +0700

    Updated widgets in the core:ui module

--- core/ui/src/main/java/com/mkirdev/unsplash/core/ui/widgets/UserImageSmall.kt
+++ core/ui/src/main/java/com/mkirdev/unsplash/core/ui/widgets/UserImageSmall.kt
@@ -1,8 +1,10 @@
 package com.mkirdev.unsplash.core.ui.widgets
 
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -11,7 +13,7 @@ import coil.request.ImageRequest
 import coil.size.Precision
 import com.mkirdev.unsplash.core.ui.R
 import com.mkirdev.unsplash.core.ui.theme.UnsplashTheme
-import com.mkirdev.unsplash.core.ui.theme.image_size_18
+import com.mkirdev.unsplash.core.ui.theme.image_size_28
 
 @Composable
 fun UserImageSmall(
@@ -21,17 +23,16 @@ fun UserImageSmall(
         model = ImageRequest.Builder(LocalContext.current)
             .data(imageUrl)
             .crossfade(true)
-            .placeholder(drawableResId = R.drawable.ic_outline_insert_photo_24)
             .precision(Precision.INEXACT)
             .build(),
         contentDescription = stringResource(id = R.string.user_image),
-        modifier = Modifier.size(image_size_18)
+        modifier = Modifier.size(image_size_28).clip(CircleShape)
     )
 }
 
 @Preview
 @Composable
-private fun UserImageSmallPreview() {
+private fun UserImageMediumPreview() {
     UnsplashTheme(dynamicColor = false) {
         UserImageSmall(imageUrl = "https://images.unsplash.com/photo-1738807992185-76ab3a0573c4?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
     }
--- core/ui/src/main/java/com/mkirdev/unsplash/core/ui/widgets/UserInfoSmall.kt
+++ core/ui/src/main/java/com/mkirdev/unsplash/core/ui/widgets/UserInfoSmall.kt
@@ -2,31 +2,44 @@ package com.mkirdev.unsplash.core.ui.widgets
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.mkirdev.unsplash.core.ui.R
 import com.mkirdev.unsplash.core.ui.theme.UnsplashTheme
+import com.mkirdev.unsplash.core.ui.theme.item_width_80
+import com.mkirdev.unsplash.core.ui.theme.padding_2
 import com.mkirdev.unsplash.core.ui.theme.space_1
-
 
 @Composable
 fun UserInfoSmall(
     name: String,
     userName: String
 ) {
-    Column(verticalArrangement = Arrangement.spacedBy(space = space_1)) {
+
+    Column(
+        modifier = Modifier.width(item_width_80).padding(bottom = padding_2),
+        verticalArrangement = Arrangement.spacedBy(space = space_1)
+    ) {
         Text(
             text = name,
             color = MaterialTheme.colorScheme.onPrimary,
-            style = MaterialTheme.typography.labelMedium
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+            style = MaterialTheme.typography.headlineSmall
         )
         Text(
             text = stringResource(id = R.string.username_format, userName),
             color = MaterialTheme.colorScheme.onPrimary,
-            style = MaterialTheme.typography.labelSmall
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+            style = MaterialTheme.typography.bodySmall
         )
     }
 }
@@ -34,10 +47,10 @@ fun UserInfoSmall(
 
 @Preview(showBackground = true)
 @Composable
-private fun ImageInfoSmallPreview() {
+private fun UserInfoMediumPreview() {
     UnsplashTheme(dynamicColor = false) {
         UserInfoSmall(
-            name = "SumUp",
+            name = "SumUp SumUpSumUp SumUpSumUpSumUpSumUpSumUpSumUp",
             userName = "sumup",
         )
     }
