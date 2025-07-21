c2024fa18bb5e9f67b199ba1973ac60ee9c28f87
MKirDev <mkir2001@mail.ru>
2025-07-21 07:58:04 +0700

    Updated api in the notification_impl module

--- features/notification_impl/src/main/java/com/mkirdev/unsplash/notification/api/NotificationFeatureApiImpl.kt
+++ features/notification_impl/src/main/java/com/mkirdev/unsplash/notification/api/NotificationFeatureApiImpl.kt
@@ -2,7 +2,7 @@ package com.mkirdev.unsplash.notification.api
 
 import androidx.compose.runtime.Composable
 import com.mkirdev.unsplash.notification.impl.NotificationScreen
-import com.mkirdev.unsplash.notification.impl.SettingScreen
+import com.mkirdev.unsplash.notification.impl.SettingsScreen
 import javax.inject.Inject
 
 class NotificationFeatureApiImpl @Inject constructor() : NotificationFeatureApi {
@@ -11,6 +11,6 @@ class NotificationFeatureApiImpl @Inject constructor() : NotificationFeatureApi 
 
 
     @Composable
-    override fun SettingsFeature(text: String): Unit = SettingScreen(text)
+    override fun SettingsFeature(text: String): Unit = SettingsScreen(text)
 
 }
