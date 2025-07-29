3a1a051e62ee8a81799d9de191ddee5430374d86
MKirDev <mkir2001@mail.ru>
2025-07-29 20:04:27 +0700

    Created repository in the domain module

---
+++ domain/src/main/java/com/mkirdev/unsplash/domain/repository/CollectionsRepository.kt
@@ -0,0 +1,16 @@
+package com.mkirdev.unsplash.domain.repository
+
+import androidx.paging.PagingData
+import com.mkirdev.unsplash.domain.models.Collection
+import com.mkirdev.unsplash.domain.models.Photo
+import kotlinx.coroutines.flow.Flow
+
+interface CollectionsRepository {
+
+    fun getCollections(): Flow<PagingData<Collection>>
+
+    suspend fun getCollectionInfo(id: String): Collection
+
+    fun getCollectionPhotos(id: String): Flow<PagingData<Photo>>
+
+}
