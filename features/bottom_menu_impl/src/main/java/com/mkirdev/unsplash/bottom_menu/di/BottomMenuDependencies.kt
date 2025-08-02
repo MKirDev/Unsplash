9ba3441e04c6467a8da095f3de9805d4f50477b4
MKirDev <mkir2001@mail.ru>
2025-08-02 23:00:56 +0700

    Updated di in the bottom_menu_impl module

--- features/bottom_menu_impl/src/main/java/com/mkirdev/unsplash/bottom_menu/di/BottomMenuDependencies.kt
+++ features/bottom_menu_impl/src/main/java/com/mkirdev/unsplash/bottom_menu/di/BottomMenuDependencies.kt
@@ -1,9 +1,12 @@
 package com.mkirdev.unsplash.bottom_menu.di
 
+import com.mkirdev.unsplash.collections.api.CollectionsFeatureApi
 import com.mkirdev.unsplash.core.navigation.TopDestinations
 import com.mkirdev.unsplash.photo_feed.api.PhotoFeedFeatureApi
 
 interface BottomMenuDependencies {
     val photoFeedFeatureApi: PhotoFeedFeatureApi
+
+    val collectionsFeatureApi: CollectionsFeatureApi
     val topLevelDestination: TopDestinations
 }
