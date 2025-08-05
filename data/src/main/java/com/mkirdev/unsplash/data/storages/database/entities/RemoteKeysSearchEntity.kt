893bd21416f4a2e3d537f4dc259b0fa93a813bd0
MKirDev <mkir2001@mail.ru>
2025-08-05 16:16:31 +0700

    Updated entities in the data module

--- data/src/main/java/com/mkirdev/unsplash/data/storages/database/entities/RemoteKeysSearchEntity.kt
+++ data/src/main/java/com/mkirdev/unsplash/data/storages/database/entities/RemoteKeysSearchEntity.kt
@@ -6,8 +6,10 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = RemoteKeysSearchEntity.TABLE_NAME)
 data class RemoteKeysSearchEntity(
-    @PrimaryKey(autoGenerate = false)
-    val id: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = PHOTO_ID)
+    val photoId: String,
     @ColumnInfo(name = PREV_PAGE)
     val prevPage: Int?,
     @ColumnInfo(name = NEXT_PAGE)
@@ -16,6 +18,7 @@ data class RemoteKeysSearchEntity(
     companion object {
         const val TABLE_NAME = "remote_keys_search"
         const val ID = "id"
+        const val PHOTO_ID = "photo_id"
         const val PREV_PAGE = "prev_page"
         const val NEXT_PAGE = "next_page"
     }
