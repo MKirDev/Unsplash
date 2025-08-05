dffdb03aee56b346b5fc074dda18200c75d0a994
MKirDev <mkir2001@mail.ru>
2025-08-05 17:19:55 +0700

    Updated api in the collections_details_impl module

--- features/collection_details_impl/src/main/java/com/mkirdev/unsplash/collection_details/api/CollectionDetailsFeatureApiImpl.kt
+++ features/collection_details_impl/src/main/java/com/mkirdev/unsplash/collection_details/api/CollectionDetailsFeatureApiImpl.kt
@@ -1,20 +1,23 @@
 package com.mkirdev.unsplash.collection_details.api
 
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavType
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
+import com.mkirdev.unsplash.collection_details.api.navigation.CollectionDetailsDestination
+import com.mkirdev.unsplash.collection_details.di.CollectionDetailsDependenciesProvider
+import com.mkirdev.unsplash.collection_details.di.DaggerCollectionDetailsComponent
 import com.mkirdev.unsplash.collection_details.impl.CollectionDetailsContract
-import com.mkirdev.unsplash.collection_details.impl.CollectionDetailsScreen
+import com.mkirdev.unsplash.collection_details.impl.CollectionDetailsScreenWrapper
 import com.mkirdev.unsplash.collection_details.impl.CollectionDetailsViewModel
 import com.mkirdev.unsplash.core.contract.viewmodel.applyEffect
-import com.mkirdev.unsplash.core.navigation.ProjectNavDestination
+import javax.inject.Inject
 
-class CollectionDetailsFeatureApiImpl : CollectionDetailsFeatureApi {
+class CollectionDetailsFeatureApiImpl @Inject constructor() : CollectionDetailsFeatureApi {
     override fun NavController.navigateToCollectionDetails(collectionId: String) {
         navigate("${CollectionDetailsDestination.route}/${collectionId}")
     }
@@ -27,8 +30,23 @@ class CollectionDetailsFeatureApiImpl : CollectionDetailsFeatureApi {
         composable(
             route = CollectionDetailsDestination.routeWithArgument,
             arguments = CollectionDetailsDestination.arguments
-        ) {
-            val viewModel: CollectionDetailsViewModel = viewModel()
+        ) { navStackEntry ->
+
+            val collectionDetailsComponent by remember {
+                mutableStateOf(
+                    DaggerCollectionDetailsComponent.builder()
+                        .addDependencies(CollectionDetailsDependenciesProvider.dependencies)
+                        .build()
+                )
+            }
+
+            val collectionId = navStackEntry.arguments?.getString(CollectionDetailsDestination.argumentName)
+                ?: throw IllegalArgumentException("Id collection is null")
+
+            val viewModel: CollectionDetailsViewModel = viewModel(
+                factory = collectionDetailsComponent.factoryAssisted.create(collectionId)
+            )
+
             val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
             viewModel.applyEffect(function = { effect ->
@@ -40,27 +58,50 @@ class CollectionDetailsFeatureApiImpl : CollectionDetailsFeatureApi {
                 }
             })
 
-            CollectionDetailsScreen(
+            CollectionDetailsScreenWrapper(
                 uiState = uiState,
-                onPhotoItemClick = { viewModel.handleEvent(CollectionDetailsContract.Event.PhotoDetailsOpenedEvent(it)) },
-                onLikeClick = { viewModel.handleEvent(CollectionDetailsContract.Event.PhotoLikedEvent(it)) },
-                onRemoveLikeClick = { viewModel.handleEvent(CollectionDetailsContract.Event.PhotoUnlikedEvent(it)) },
-                onDownloadClick = { viewModel.handleEvent(CollectionDetailsContract.Event.DownloadRequestedEvent(it)) },
-                onLoadError = { viewModel.handleEvent(CollectionDetailsContract.Event.LoadingErrorEvent)},
+                onPhotoItemClick = {
+                    viewModel.handleEvent(
+                        CollectionDetailsContract.Event.PhotoDetailsOpenedEvent(
+                            it
+                        )
+                    )
+                },
+                onLikeClick = {
+                    viewModel.handleEvent(
+                        CollectionDetailsContract.Event.PhotoLikedEvent(
+                            it
+                        )
+                    )
+                },
+                onRemoveLikeClick = {
+                    viewModel.handleEvent(
+                        CollectionDetailsContract.Event.PhotoUnlikedEvent(
+                            it
+                        )
+                    )
+                },
+                onDownloadClick = {
+                    viewModel.handleEvent(
+                        CollectionDetailsContract.Event.DownloadRequestedEvent(
+                            it
+                        )
+                    )
+                },
+                onLoadError = { viewModel.handleEvent(CollectionDetailsContract.Event.LoadingErrorEvent) },
                 onCloseFieldClick = { viewModel.handleEvent(CollectionDetailsContract.Event.FieldClosedEvent) },
                 onPagingCloseFieldClick = { viewModel.handleEvent(CollectionDetailsContract.Event.PagingFieldClosedEvent) },
+                onPagingRetry = {
+                    viewModel.handleEvent(
+                        CollectionDetailsContract.Event.PagingRetryEvent(
+                            it
+                        )
+                    )
+                },
                 onNavigateUp = { viewModel.handleEvent(CollectionDetailsContract.Event.NavigateUpEvent) },
                 onNavigateBack = { viewModel.handleEvent(CollectionDetailsContract.Event.NavigateBackEvent) }
             )
         }
     }
 }
-
-object CollectionDetailsDestination : ProjectNavDestination {
-    private const val argumentName = "collectionId"
-    override val route: String = "collection_details"
-    val routeWithArgument = "${route}/{${argumentName}}"
-    val arguments = listOf(navArgument(argumentName) { type = NavType.StringType })
-}
-
 
