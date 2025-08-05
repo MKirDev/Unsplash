7b484da9f3c6127945a3e98f11b5b3663496d079
MKirDev <mkir2001@mail.ru>
2025-08-05 17:22:39 +0700

    Updated App.kt in the app module

--- app/src/main/java/com/mkirdev/unsplash/app/App.kt
+++ app/src/main/java/com/mkirdev/unsplash/app/App.kt
@@ -9,6 +9,7 @@ import androidx.datastore.preferences.preferencesDataStore
 import androidx.work.WorkManager
 import com.mkirdev.unsplash.auth.di.AuthDependenciesProvider
 import com.mkirdev.unsplash.bottom_menu.di.BottomMenuDependenciesProvider
+import com.mkirdev.unsplash.collection_details.di.CollectionDetailsDependenciesProvider
 import com.mkirdev.unsplash.collections.di.CollectionsDependenciesProvider
 import com.mkirdev.unsplash.details.di.DetailsDependenciesProvider
 import com.mkirdev.unsplash.di.DaggerAppComponent
@@ -58,6 +59,7 @@ class App : Application() {
         PhotoFeedDependenciesProvider.dependencies = appComponent
         DetailsDependenciesProvider.dependencies = appComponent
         CollectionsDependenciesProvider.dependencies = appComponent
+        CollectionDetailsDependenciesProvider.dependencies = appComponent
     }
 
     private fun launchCollectors() {
