b62e9489388fcbefe5b155c4f5461247b28e71d0
MKirDev <mkir2001@mail.ru>
2025-08-08 03:50:02 +0700

    Created CurrentUserNetwork.kt in the data module

---
+++ data/src/main/java/com/mkirdev/unsplash/data/network/models/common/CurrentUserNetwork.kt
@@ -0,0 +1,28 @@
+package com.mkirdev.unsplash.data.network.models.common
+
+import com.google.gson.annotations.SerializedName
+
+data class CurrentUserNetwork(
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("username")
+    val username: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("first_name")
+    val firstName: String,
+    @SerializedName("last_name")
+    val lastName: String,
+    @SerializedName("profile_image")
+    val imageUrl: ProfileImageNetwork,
+    @SerializedName("total_likes")
+    val totalLikes: Int,
+    @SerializedName("bio")
+    val bio: String?,
+    @SerializedName("location")
+    val location: String?,
+    @SerializedName("downloads")
+    val downloads: Int?,
+    @SerializedName("email")
+    val email: String?
+)
