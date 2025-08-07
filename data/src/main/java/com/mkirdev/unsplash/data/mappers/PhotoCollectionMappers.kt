cf7b47c012c0dd41e5dcd0aa02a4d6299d0ff334
MKirDev <mkir2001@mail.ru>
2025-08-08 03:30:57 +0700

    Updated mappers in the data module

--- data/src/main/java/com/mkirdev/unsplash/data/mappers/PhotoCollectionMappers.kt
+++ data/src/main/java/com/mkirdev/unsplash/data/mappers/PhotoCollectionMappers.kt
@@ -1,17 +1,16 @@
 package com.mkirdev.unsplash.data.mappers
 
 import com.mkirdev.unsplash.data.network.models.list.PhotoCollectionNetwork
-import com.mkirdev.unsplash.data.network.models.list.PhotoFeedNetwork
+import com.mkirdev.unsplash.data.storages.database.dto.collection.PhotoCollectionJoinedDto
 import com.mkirdev.unsplash.data.storages.database.dto.collection.PhotoFromCollectionDto
-import com.mkirdev.unsplash.data.storages.database.entities.PhotoCollectionEntity
-import com.mkirdev.unsplash.data.storages.database.entities.PhotoEntity
-import com.mkirdev.unsplash.data.storages.database.entities.PhotoReactionsEntity
-import com.mkirdev.unsplash.data.storages.database.entities.ReactionsTypeEntity
 import com.mkirdev.unsplash.data.storages.database.entities.RemoteKeysCollectionEntity
+import com.mkirdev.unsplash.data.storages.database.entities.collection.PhotoCollectionEntity
+import com.mkirdev.unsplash.data.storages.database.entities.collection.PhotoReactionsCollectionEntity
+import com.mkirdev.unsplash.data.storages.database.entities.collection.ReactionsCollectionEntity
 import com.mkirdev.unsplash.domain.models.Links
 import com.mkirdev.unsplash.domain.models.Photo
 
-internal fun PhotoCollectionNetwork.toKeysEntity(prevPage: Int?, nextPage: Int?) : RemoteKeysCollectionEntity {
+internal fun PhotoCollectionNetwork.toKeysCollectionEntity(prevPage: Int?, nextPage: Int?) : RemoteKeysCollectionEntity {
     return RemoteKeysCollectionEntity(
         photoId = id,
         prevPage = prevPage,
@@ -19,21 +18,21 @@ internal fun PhotoCollectionNetwork.toKeysEntity(prevPage: Int?, nextPage: Int?)
     )
 }
 
-internal fun PhotoCollectionNetwork.toReactionsTypeEntity(): ReactionsTypeEntity {
-    return ReactionsTypeEntity(
-        id = id,
+internal fun PhotoCollectionNetwork.toReactionsCollectionEntity(): ReactionsCollectionEntity {
+    return ReactionsCollectionEntity(
+        photoId = id,
         liked = if (likedByUser) 1 else 0
     )
 }
 
-internal fun PhotoCollectionNetwork.toPhotoReactionsEntity(): PhotoReactionsEntity {
-    return PhotoReactionsEntity(
+internal fun PhotoCollectionNetwork.toPhotoReactionsCollectionEntity(): PhotoReactionsCollectionEntity {
+    return PhotoReactionsCollectionEntity(
         photoId = id,
         reactionsId = id
     )
 }
 
-internal fun PhotoCollectionNetwork.toPhotoEntity(position: Int): PhotoEntity {
+internal fun PhotoCollectionNetwork.toPhotoCollectionEntity(collectionId: String): PhotoCollectionEntity {
     val displayWidth = when (width) {
         in 0..2500 -> 180
         in 2501..4000 -> 240
@@ -45,8 +44,8 @@ internal fun PhotoCollectionNetwork.toPhotoEntity(position: Int): PhotoEntity {
         else -> 350
     }
 
-    return PhotoEntity(
-        id = id,
+    return PhotoCollectionEntity(
+        photoId = id,
         width = displayWidth,
         height = displayHeight,
         imageUrl = imageUrl.toRegular(),
@@ -54,16 +53,27 @@ internal fun PhotoCollectionNetwork.toPhotoEntity(position: Int): PhotoEntity {
         htmlLink = links.toHtml(),
         likes = likes,
         userId = user.id,
-        searchType = 0,
-        position = position
+        collectionId = collectionId
     )
 }
 
-internal fun PhotoCollectionNetwork.toPhotoCollectionEntity(collectionId: String) =
-    PhotoCollectionEntity(
-        photoId = id,
-        collectionId = collectionId
+internal fun PhotoCollectionJoinedDto.toDomain(): Photo {
+    return Photo(
+        position = position,
+        id = photoId,
+        width = width,
+        height = height,
+        imageUrl = imageUrl,
+        links = Links(html = htmlLink, download = downloadLink),
+        likes = likes,
+        likedByUser = likedByUser == 1,
+        user = userFeedDto.toDomain(),
+        location = null,
+        exif = null,
+        tags = null,
+        downloads = null,
     )
+}
 
 internal fun PhotoFromCollectionDto.toDomain() =
     Photo(
@@ -98,6 +108,7 @@ internal fun PhotoCollectionNetwork.toDomain() =
         downloads = 0,
         position = 0
     )
+
 
 
 
