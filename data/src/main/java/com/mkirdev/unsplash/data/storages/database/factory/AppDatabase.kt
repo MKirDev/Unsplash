e35f39ab87c56c7a076b9de724bd97e0870668be
MKirDev <mkir2001@mail.ru>
2025-08-08 03:59:31 +0700

    Updated AppDatabase.kt in the data module

--- data/src/main/java/com/mkirdev/unsplash/data/storages/database/factory/AppDatabase.kt
+++ data/src/main/java/com/mkirdev/unsplash/data/storages/database/factory/AppDatabase.kt
@@ -2,67 +2,100 @@ package com.mkirdev.unsplash.data.storages.database.factory
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.mkirdev.unsplash.data.storages.database.dao.base.CollectionDao
-import com.mkirdev.unsplash.data.storages.database.dao.base.PhotoCollectionDao
-import com.mkirdev.unsplash.data.storages.database.dao.base.PhotoDao
-import com.mkirdev.unsplash.data.storages.database.dao.base.PhotoReactionsDao
-import com.mkirdev.unsplash.data.storages.database.dao.base.ReactionsTypeDao
-import com.mkirdev.unsplash.data.storages.database.dao.base.RemoteKeysCollectionDao
-import com.mkirdev.unsplash.data.storages.database.dao.base.RemoteKeysFeedDao
-import com.mkirdev.unsplash.data.storages.database.dao.base.RemoteKeysSearchDao
-import com.mkirdev.unsplash.data.storages.database.dao.base.UserDao
-import com.mkirdev.unsplash.data.storages.database.dao.collection.PhotoFromCollectionDao
+import com.mkirdev.unsplash.data.storages.database.dao.collection.RemoteKeysCollectionDao
+import com.mkirdev.unsplash.data.storages.database.dao.feed.RemoteKeysFeedDao
+import com.mkirdev.unsplash.data.storages.database.dao.search.RemoteKeysSearchDao
+import com.mkirdev.unsplash.data.storages.database.dao.collection.PhotoCollectionDao
+import com.mkirdev.unsplash.data.storages.database.dao.collection.PhotoCollectionJoinedDao
+import com.mkirdev.unsplash.data.storages.database.dao.collection.PhotoReactionsCollectionDao
+import com.mkirdev.unsplash.data.storages.database.dao.collection.ReactionsCollectionDao
+import com.mkirdev.unsplash.data.storages.database.dao.collection.UserCollectionDao
 import com.mkirdev.unsplash.data.storages.database.dao.feed.PhotoFeedDao
+import com.mkirdev.unsplash.data.storages.database.dao.feed.PhotoFeedJoinedDao
+import com.mkirdev.unsplash.data.storages.database.dao.feed.PhotoReactionsFeedDao
+import com.mkirdev.unsplash.data.storages.database.dao.feed.ReactionsFeedDao
+import com.mkirdev.unsplash.data.storages.database.dao.feed.UserFeedDao
+import com.mkirdev.unsplash.data.storages.database.dao.search.PhotoReactionsSearchDao
 import com.mkirdev.unsplash.data.storages.database.dao.search.PhotoSearchDao
-import com.mkirdev.unsplash.data.storages.database.entities.CollectionEntity
-import com.mkirdev.unsplash.data.storages.database.entities.PhotoCollectionEntity
-import com.mkirdev.unsplash.data.storages.database.entities.PhotoEntity
-import com.mkirdev.unsplash.data.storages.database.entities.PhotoReactionsEntity
-import com.mkirdev.unsplash.data.storages.database.entities.ReactionsTypeEntity
-import com.mkirdev.unsplash.data.storages.database.entities.RemoteKeysCollectionEntity
-import com.mkirdev.unsplash.data.storages.database.entities.RemoteKeysFeedEntity
-import com.mkirdev.unsplash.data.storages.database.entities.RemoteKeysSearchEntity
-import com.mkirdev.unsplash.data.storages.database.entities.UserEntity
+import com.mkirdev.unsplash.data.storages.database.dao.search.PhotoSearchJoinedDao
+import com.mkirdev.unsplash.data.storages.database.dao.search.ReactionsSearchDao
+import com.mkirdev.unsplash.data.storages.database.dao.search.UserSearchDao
+import com.mkirdev.unsplash.data.storages.database.entities.collection.RemoteKeysCollectionEntity
+import com.mkirdev.unsplash.data.storages.database.entities.feed.RemoteKeysFeedEntity
+import com.mkirdev.unsplash.data.storages.database.entities.search.RemoteKeysSearchEntity
+import com.mkirdev.unsplash.data.storages.database.entities.collection.PhotoCollectionEntity
+import com.mkirdev.unsplash.data.storages.database.entities.collection.PhotoReactionsCollectionEntity
+import com.mkirdev.unsplash.data.storages.database.entities.collection.ReactionsCollectionEntity
+import com.mkirdev.unsplash.data.storages.database.entities.collection.UserCollectionEntity
+import com.mkirdev.unsplash.data.storages.database.entities.feed.PhotoFeedEntity
+import com.mkirdev.unsplash.data.storages.database.entities.feed.PhotoReactionsFeedEntity
+import com.mkirdev.unsplash.data.storages.database.entities.feed.ReactionsFeedEntity
+import com.mkirdev.unsplash.data.storages.database.entities.feed.UserFeedEntity
+import com.mkirdev.unsplash.data.storages.database.entities.search.PhotoReactionsSearchEntity
+import com.mkirdev.unsplash.data.storages.database.entities.search.PhotoSearchEntity
+import com.mkirdev.unsplash.data.storages.database.entities.search.ReactionsSearchEntity
+import com.mkirdev.unsplash.data.storages.database.entities.search.UserSearchEntity
 
 private const val DATABASE_VERSION = 1
 
 @Database(
     entities = [
-        CollectionEntity::class,
-        PhotoCollectionEntity::class,
-        PhotoEntity::class,
-        PhotoReactionsEntity::class,
-        ReactionsTypeEntity::class,
-        UserEntity::class,
+        PhotoFeedEntity::class,
+        UserFeedEntity::class,
+        ReactionsFeedEntity::class,
+        PhotoReactionsFeedEntity::class,
         RemoteKeysFeedEntity::class,
+        PhotoSearchEntity::class,
+        UserSearchEntity::class,
+        ReactionsSearchEntity::class,
+        PhotoReactionsSearchEntity::class,
         RemoteKeysSearchEntity::class,
-        RemoteKeysCollectionEntity::class
+        PhotoCollectionEntity::class,
+        UserCollectionEntity::class,
+        ReactionsCollectionEntity::class,
+        PhotoReactionsCollectionEntity::class,
+        RemoteKeysCollectionEntity::class,
     ],
     version = DATABASE_VERSION
 )
 abstract class AppDatabase : RoomDatabase() {
 
-    abstract fun collectionDao(): CollectionDao
-
-    abstract fun photoCollectionDao(): PhotoCollectionDao
-
-    abstract fun photoDao(): PhotoDao
-
-    abstract fun photoReactionsDao(): PhotoReactionsDao
-
-    abstract fun reactionsTypeDao(): ReactionsTypeDao
-
-    abstract fun userDao(): UserDao
-
-    abstract fun photoFromCollectionDao(): PhotoFromCollectionDao
-
     abstract fun photoFeedDao(): PhotoFeedDao
 
-    abstract fun photoSearchDao(): PhotoSearchDao
+    abstract fun photoFeedJoinedDao(): PhotoFeedJoinedDao
+
+    abstract fun photoReactionsFeedDao(): PhotoReactionsFeedDao
+
+    abstract fun reactionsFeedDao(): ReactionsFeedDao
+
+    abstract fun userFeedDao(): UserFeedDao
 
     abstract fun remoteKeysFeedDao(): RemoteKeysFeedDao
 
+
+    abstract fun photoSearchDao(): PhotoSearchDao
+
+    abstract fun photoSearchJoinedDao(): PhotoSearchJoinedDao
+
+    abstract fun photoReactionsSearchDao(): PhotoReactionsSearchDao
+
+    abstract fun reactionsSearchDao(): ReactionsSearchDao
+
+    abstract fun userSearchDao(): UserSearchDao
+
     abstract fun remoteKeysSearchDao(): RemoteKeysSearchDao
 
+
+    abstract fun photoCollectionDao(): PhotoCollectionDao
+
+    abstract fun photoCollectionJoinedDao(): PhotoCollectionJoinedDao
+
+    abstract fun photoReactionsCollectionDao(): PhotoReactionsCollectionDao
+
+    abstract fun reactionsCollectionDao(): ReactionsCollectionDao
+
+    abstract fun userCollectionDao(): UserCollectionDao
+
     abstract fun remoteKeysCollectionDao(): RemoteKeysCollectionDao
+
 }
