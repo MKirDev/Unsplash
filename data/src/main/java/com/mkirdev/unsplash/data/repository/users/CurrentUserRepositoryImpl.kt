d79fcf78758898f872ea07a5d9464394ffc7653d
MKirDev <mkir2001@mail.ru>
2025-08-11 13:38:50 +0700

    Updated user repositoty in the data module

--- data/src/main/java/com/mkirdev/unsplash/data/repository/users/CurrentUserRepositoryImpl.kt
+++ data/src/main/java/com/mkirdev/unsplash/data/repository/users/CurrentUserRepositoryImpl.kt
@@ -1,17 +1,20 @@
 package com.mkirdev.unsplash.data.repository.users
 
+import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.map
-import com.mkirdev.unsplash.data.exceptions.UsersException
+import com.mkirdev.unsplash.data.exceptions.UserException
 import com.mkirdev.unsplash.data.mappers.toDomain
-import com.mkirdev.unsplash.data.network.models.list.PhotoFeedNetwork
+import com.mkirdev.unsplash.data.mappers.toDto
+import com.mkirdev.unsplash.data.mappers.toUserProfileEntity
 import com.mkirdev.unsplash.data.network.photos.api.CurrentUserApi
-import com.mkirdev.unsplash.data.paging.UserPhotosPagingSource
+import com.mkirdev.unsplash.data.paging.PhotoLikedRemoteMediator
+import com.mkirdev.unsplash.data.storages.database.factory.AppDatabase
+import com.mkirdev.unsplash.data.storages.database.view.PhotoLikedJoinedView
 import com.mkirdev.unsplash.domain.models.CurrentUser
 import com.mkirdev.unsplash.domain.models.Photo
-import com.mkirdev.unsplash.domain.models.User
 import com.mkirdev.unsplash.domain.repository.CurrentUserRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
@@ -22,33 +25,54 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-private const val ITEMS_PER_PAGE = 20
+private const val ITEMS_PER_PAGE = 10
 
+@OptIn(ExperimentalPagingApi::class)
 class CurrentUserRepositoryImpl @Inject constructor(
     private val currentUserApi: CurrentUserApi,
+    private val appDatabase: AppDatabase,
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : CurrentUserRepository {
     override fun getLikedPhotos(username: String): Flow<PagingData<Photo>> {
         return Pager(
             config = PagingConfig(pageSize = ITEMS_PER_PAGE),
+            remoteMediator = PhotoLikedRemoteMediator(
+                username = username,
+                currentUserApi = currentUserApi,
+                appDatabase = appDatabase
+            ),
             pagingSourceFactory = {
-                UserPhotosPagingSource(
-                    username = username,
-                    currentUserApi = currentUserApi
-                )
+                appDatabase.photoLikedJoinedDao().getLikedJoinedPhotos()
             }
-        ).flow.map { value: PagingData<PhotoFeedNetwork> ->
-            value.map { photoFeedNetwork -> photoFeedNetwork.toDomain() }
+        ).flow.map { value: PagingData<PhotoLikedJoinedView> ->
+            value.map { photoLikedJoinedView -> photoLikedJoinedView.toDto().toDomain() }
         }.flowOn(dispatcher).catch {
-            throw UsersException.GetLikedPhotosException(it)
+            throw UserException.GetLikedPhotosException(it)
         }
     }
 
-    override suspend fun getUserInfo(): CurrentUser = withContext(dispatcher) {
+    override suspend fun getCurrentUser(): CurrentUser = withContext(dispatcher) {
         try {
-            currentUserApi.getUserInfo().toDomain()
+            appDatabase.userProfileDao().getUser()?.toDomain() ?: throw Throwable()
         } catch (t: Throwable) {
-            throw UsersException.GetUserInfoException(t)
+            throw UserException.GetCurrentUserException(t)
+        }
+    }
+
+    override suspend fun addCurrentUser() = withContext(dispatcher) {
+        try {
+            val user = currentUserApi.getCurrentUser().toUserProfileEntity()
+            appDatabase.userProfileDao().addUser(user)
+        } catch (t: Throwable) {
+            throw UserException.AddCurrentUserException(t)
+        }
+    }
+
+    override suspend fun clearUserFromDatabase() = withContext(dispatcher) {
+        try {
+            appDatabase.userProfileDao().deleteUser()
+        } catch (t: Throwable) {
+            throw UserException.ClearUserFromDatabase(t)
         }
     }
 }
