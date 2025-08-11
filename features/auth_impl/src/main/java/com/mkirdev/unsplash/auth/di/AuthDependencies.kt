8d71b08a66b2b453501e5c62fb2aaa3978c8916f
MKirDev <mkir2001@mail.ru>
2025-08-11 13:39:50 +0700

    Updated di in the auth_impl module

--- features/auth_impl/src/main/java/com/mkirdev/unsplash/auth/di/AuthDependencies.kt
+++ features/auth_impl/src/main/java/com/mkirdev/unsplash/auth/di/AuthDependencies.kt
@@ -1,9 +1,16 @@
 package com.mkirdev.unsplash.auth.di
 
 import com.mkirdev.unsplash.domain.repository.AuthRepository
+import com.mkirdev.unsplash.domain.usecases.preferences.DeleteScheduleFlagUseCase
+import com.mkirdev.unsplash.domain.usecases.preferences.SaveScheduleFlagUseCase
+import com.mkirdev.unsplash.domain.usecases.user.AddCurrentUserUseCase
 import net.openid.appauth.AuthorizationService
 
 interface AuthDependencies {
     val authRepository: AuthRepository
     val authService: AuthorizationService
+
+    val addCurrentUserUseCase: AddCurrentUserUseCase
+
+    val deleteScheduleFlagUseCase: DeleteScheduleFlagUseCase
 }
