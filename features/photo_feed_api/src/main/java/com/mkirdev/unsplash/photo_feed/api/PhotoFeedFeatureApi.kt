0e949ff3d7d070c4ab66ff2cdfd7091240cdec4d
MKirDev <mkir2001@mail.ru>
2025-09-09 18:00:16 +0700

    Updated navigation and api in the photo_feed_api module

--- features/photo_feed_api/src/main/java/com/mkirdev/unsplash/photo_feed/api/PhotoFeedFeatureApi.kt
+++ features/photo_feed_api/src/main/java/com/mkirdev/unsplash/photo_feed/api/PhotoFeedFeatureApi.kt
@@ -1,7 +1,13 @@
 package com.mkirdev.unsplash.photo_feed.api
 
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
 
 interface PhotoFeedFeatureApi {
-    fun NavGraphBuilder.photoFeed(onNavigateToDetails: (String) -> Unit)
+
+    fun NavHostController.navigateToFeed()
+    fun NavGraphBuilder.photoFeed(
+        onNavigateToDetails: (String) -> Unit,
+        onNavigateToSearch: () -> Unit
+    )
 }
---
+++ features/photo_feed_api/src/main/java/com/mkirdev/unsplash/photo_feed/api/navigation/PhotoFeedDestination.kt
@@ -0,0 +1,8 @@
+package com.mkirdev.unsplash.photo_feed.api.navigation
+
+import com.mkirdev.unsplash.core.navigation.ProjectNavDestination
+
+
+object PhotoFeedDestination : ProjectNavDestination {
+    override val route: String = "photo_feed"
+}
